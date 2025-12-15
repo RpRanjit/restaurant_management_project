7d69f1bf5d941553af8cc34a08fa413fb51a3529
Ranjit Poudel <ranjitsparrow2@gmail.com>
2025-12-15 23:14:43 +0545

    Adding a note module and make it authenticative so that only the login_in and the user can see it

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Note
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+@login_required
+def note_list(request):
+    notes = Note.objects.filter(user = request.user)
+    return render(request, "home/note_list.html", {"notes": notes})
